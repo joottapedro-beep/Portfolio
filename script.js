418c6d49561f21a25bfc99356c7de12c733412b9
joottapedro-beep <joottapedro@gmail.com>
2026-03-30 23:28:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<script>
 const obs = new IntersectionObserver(entries => {
   entries.forEach(e => { if(e.isIntersecting){e.target.classList.add('visible');obs.unobserve(e.target);} });
 },{threshold:0.08});
